dec629bf7729cb0a2163cb1e4361797416602f34
George Pamfilis <gpamfilis@gmail.com>
2017-10-18 18:20:16 +0300

    added requirements and runtime

--- webapp.py
+++ webapp.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'hard pennis poop'
+    return 'Welcome to my vagina!'
+
+
+@app.route('/pump-valve-setup')
+def pump_valve_setup():
+    return jsonify({'test': 0})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
